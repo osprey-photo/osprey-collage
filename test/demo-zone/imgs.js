a9b5b8f652145432a2653911e86a282619fdc522
mbw <matthew@mh-white.com>
2019-04-12 08:21:48 +0000

    feat: test composition
    Signed-off-by: mbw <matthew@mh-white.com>

--- test/demo-zone/imgs.js
+++ test/demo-zone/imgs.js
@@ -1,13 +1,10 @@
 
 const path = require('path');
-
 var Jimp = require('jimp');
 
-
-
-const createImage = async () => {
+const createImage = async (width=200,height=200,bkg='#FFFFFF') => {
 	return new Promise((resolve,reject)=>{
-		new Jimp(200,200, '#FF00FF' , (err, image) => {
+		new Jimp(width, height, bkg , (err, image) => {
 			if (image){		
 				resolve(image);
 			} else {
@@ -17,18 +14,41 @@ const createImage = async () => {
 	})
 }
 
-const main = async () => {
+const makeTestImages = async () => {
 
    const font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
 
    for (let i =0; i<10; i++){
-   	  let image = await createImage();
+   	  let image = await createImage(200,200,'#FF00FF');
    	  image.print(font,10,10,`Image ${i}`);
+   	  console.log(`Writing test imgage ${i}`);
    	  await image.writeAsync(`./imgs/image${i}.png`);
    }
 	
 }
 
+let finalImage = {
+	width:600,
+	height:600
+}
+
+const main = async () => {
+
+	//await makeTestImages();   
+	// create the large image
+	finalImage.image = await createImage(finalImage.width,finalImage.height);
+	// load the images
+
+	let images = [];
+	for (let i=0; i<9; i++){
+		let img = await Jimp.read(`./imgs/image${i}.png`);
+		let x = (i % 3)*200;
+		let y = (~~(i / 3))*200;
+		console.log(`Image ${i} at ${x} : ${y}`)
+		await finalImage.image.composite(img,x,y)
+	}
+	await finalImage.image.writeAsync('./imgs/final.png');
+}
 
 // run main
 main().then(()=>{
